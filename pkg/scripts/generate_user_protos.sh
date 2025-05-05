7339948649bdb91020c370e5c59f395d15f933d9
ruziba3vich <rruzibayevich@gmail.com>
2025-05-05 18:02:35 +0500

    fix user_service: fix proto generato script path

--- pkg/scripts/generate_user_protos.sh
+++ pkg/scripts/generate_user_protos.sh
@@ -12,6 +12,6 @@ mkdir -p $OUT_DIR
 protoc -I=$PROTO_DIR \
   --go_out=$OUT_DIR \
   --go-grpc_out=$OUT_DIR \
-  $PROTO_DIR/compiler_protos/compiler.proto
+  $PROTO_DIR/mm_user_protos/user.proto
 
 echo "protos generated successfully"
