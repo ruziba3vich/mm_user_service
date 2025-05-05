65db59af1618415075cc8279d021fefe5743fb2c
ruziba3vich <rruzibayevich@gmail.com>
2025-05-06 02:18:33 +0500

    feat user_service: create refresh token model

--- internal/models/modles.go
+++ internal/models/modles.go
@@ -2,9 +2,22 @@ package models
 
 import "time"
 
-type User struct {
-	ID           string    `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
-	Username     string    `gorm:"unique;not null"`
-	PasswordHash string    `gorm:"not null"`
-	CreatedAt    time.Time `gorm:"autoCreateTime"`
-}
+type (
+	User struct {
+		ID           string    `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
+		FullName     string    `gorm:"not null"`
+		Username     string    `gorm:"unique;not null"`
+		PasswordHash string    `gorm:"not null"`
+		CreatedAt    time.Time `gorm:"autoCreateTime"`
+	}
+
+	RefreshToken struct {
+		ID         string    `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
+		UserID     string    `gorm:"type:uuid;not null;index"`
+		TokenHash  string    `gorm:"not null"`
+		DeviceID   string    `gorm:"not null"`
+		CreatedAt  time.Time `gorm:"autoCreateTime"`
+		ExpiresAt  time.Time `gorm:"not null"`
+		LastUsedAt time.Time
+	}
+)
