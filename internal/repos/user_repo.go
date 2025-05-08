e1549acee9ce4b9edc35fe096e85d724fe0eff46
ruziba3vich <rruzibayevich@gmail.com>
2025-05-09 03:11:24 +0500

    fix user_service: fix repos

--- internal/repos/user_repo.go
+++ internal/repos/user_repo.go
@@ -4,15 +4,16 @@ import (
 	"context"
 
 	"github.com/ruziba3vich/mm_user_service/genprotos/genprotos/user_protos"
+	"github.com/ruziba3vich/mm_user_service/internal/models"
 )
 
 type (
 	UserRepo interface {
-		SignUp(context.Context, *user_protos.SignUpRequest) (*user_protos.SignUpResponse, error)
-		Login(context.Context, *user_protos.LoginRequest) (*user_protos.LoginResponse, error)
+		CreateUser(ctx context.Context, user *models.User) (*models.User, error)
+		ValidateUserCredentials(ctx context.Context, username string, passwordHash string) (*models.User, error)
 		Logout(context.Context, *user_protos.LogoutRequest) (*user_protos.LogoutResponse, error)
 		RefreshToken(context.Context, *user_protos.RefreshTokenRequest) (*user_protos.RefreshTokenResponse, error)
-		AddProfilePicture(context.Context, *user_protos.AddProfilePictureRequest) (*user_protos.AddProfilePictureResponse, error)
+		AddProfilePicture(ctx context.Context, userId string, fileName string, pictureId string) error
 		RemoveProfilePicture(context.Context, *user_protos.RemoveProfilePictureRequest) (*user_protos.RemoveProfilePictureResponse, error)
 		UpdateUser(context.Context, *user_protos.UpdateUserRequest) (*user_protos.UpdateUserResponse, error)
 		GetUserById(context.Context, *user_protos.GetUserByIdRequest) (*user_protos.GetUserByIdResponse, error)
