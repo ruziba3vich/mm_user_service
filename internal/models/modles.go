6343d8804db671355880433ac86e596d1827b6d9
ruziba3vich <rruzibayevich@gmail.com>
2025-05-09 01:37:51 +0500

    fix user_service: fix models

--- internal/models/modles.go
+++ internal/models/modles.go
@@ -4,11 +4,12 @@ import "time"
 
 type (
 	User struct {
-		ID           string    `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
-		FullName     string    `gorm:"not null"`
-		Username     string    `gorm:"unique;not null"`
-		PasswordHash string    `gorm:"not null"`
-		CreatedAt    time.Time `gorm:"autoCreateTime"`
+		ID             string    `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
+		FullName       string    `gorm:"not null"`
+		Username       string    `gorm:"unique;not null"`
+		PasswordHash   string    `gorm:"not null"`
+		CreatedAt      time.Time `gorm:"autoCreateTime"`
+		FollowersCount int       `gorm:"default:0"`
 	}
 
 	RefreshToken struct {
@@ -22,7 +23,14 @@ type (
 	}
 
 	ProfilePicture struct {
+		ID       string `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
 		FileName string `gorm:"not null"`
-		UserID   string `gorm:"not null"`
+		UserID   string `gorm:"type:uuid;not null;index"`
+	}
+
+	Followings struct {
+		ID        string `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
+		Following string `gorm:"type:uuid;not null;index"`
+		Follower  string `gorm:"type:uuid;not null;index"`
 	}
 )
