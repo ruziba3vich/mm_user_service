6b02dca1afa9aed3f875275794d27fb930b1ea9c
ruziba3vich <rruzibayevich@gmail.com>
2025-05-09 06:41:52 +0500

    feat user_service: impelement file storage

---
+++ internal/storage/file_storage.go
@@ -0,0 +1,70 @@
+package storage
+
+import (
+	"bytes"
+	"context"
+	"path/filepath"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+	"github.com/ruziba3vich/mm_article_service/pkg/config"
+)
+
+type MinioStorage struct {
+	client     *minio.Client
+	bucketName string
+	urlExpiry  int64
+}
+
+func NewMinIOStorage(cfg *config.Config) (*MinioStorage, error) {
+	client, err := minio.New(cfg.MinIO.Endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(cfg.MinIO.AccessKey, cfg.MinIO.SecretKey, ""),
+		Secure: false,
+	})
+	if err != nil {
+		return nil, err
+	}
+	exists, err := client.BucketExists(context.Background(), cfg.MinIO.Bucket)
+	if err != nil {
+		return nil, err
+	}
+	if !exists {
+		err = client.MakeBucket(context.Background(), cfg.MinIO.Bucket, minio.MakeBucketOptions{})
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &MinioStorage{
+		client:     client,
+		bucketName: cfg.MinIO.Bucket,
+		urlExpiry:  int64(cfg.MinIO.UrlExpiry),
+	}, nil
+}
+
+func (s *MinioStorage) CreateFile(ctx context.Context, fileName string, fileContent []byte) (string, string, error) {
+	ext := filepath.Ext(fileName)
+	generatedName := uuid.New().String() + ext
+	_, err := s.client.PutObject(ctx, s.bucketName, generatedName, bytes.NewReader(fileContent), int64(len(fileContent)), minio.PutObjectOptions{})
+	if err != nil {
+		return "", "", err
+	}
+	url, err := s.GetFileURL(ctx, generatedName)
+	if err != nil {
+		return "", "", err
+	}
+	return generatedName, url, nil
+}
+
+func (s *MinioStorage) DeleteFile(ctx context.Context, fileName string) error {
+	return s.client.RemoveObject(ctx, s.bucketName, fileName, minio.RemoveObjectOptions{})
+}
+
+func (s *MinioStorage) GetFileURL(ctx context.Context, fileName string) (string, error) {
+	url, err := s.client.PresignedGetObject(ctx, s.bucketName, fileName, time.Duration(s.urlExpiry)*time.Second, nil)
+	if err != nil {
+		return "", err
+	}
+	return url.String(), nil
+}
